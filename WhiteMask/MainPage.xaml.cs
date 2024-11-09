9f624912139808fe8da1932c8583ac66e2ad12fb
Ect07 <ect07aha@hotmail.com>
2024-11-09 20:05:33 +0800

    Add project files.

---
+++ WhiteMask/MainPage.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace WhiteMask {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WhiteMask/Platforms/Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Content.Res;
+using Android.OS;
+
+namespace WhiteMask {
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity {
+
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			// 获取窗口
+			var window = Window;
+
+			// 检查当前系统是否为暗色模式
+			bool isDarkTheme = (Resources.Configuration.UiMode & UiMode.NightMask) == UiMode.NightYes;
+
+			// 设置状态栏颜色
+			if (isDarkTheme) {
+				window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#000000"));
+				window.SetNavigationBarColor(Android.Graphics.Color.ParseColor("#000000"));
+			} else {
+				window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#477ee7"));
+				window.SetNavigationBarColor(Android.Graphics.Color.ParseColor("#9ebfff"));
+			}
+		}
+	}
+}
